276439595c4b45b56255f24142fb90e11f096ad0
Pierre Tessier <pierre.borie.tessier@gmail.com>
2021-12-15 15:05:52 -0500

    complete experiments

--- enum.r
+++ enum.r
@@ -5,19 +5,47 @@ library(ggplot2)
 library(patchwork)
 library(scales)
 
-data("fl25_enum")
 data("fl25")
 
-map = redist_map(fl25, ndists = 3, pop_tol=0.1, total_pop = pop)
+pop_tol <- 0.25
+n <- 4
+mid <- 0.5
+map = redist_map(fl25, ndists = n, pop_tol=pop_tol, total_pop = pop)
 redist.plot.map(map, fill = obama / (obama + mccain)) + 
   scale_fill_gradient2(midpoint=mid, mid="white" , low="red", high="blue", space ="Lab") +
   labs(color="Democract vote share", title = "Democrat vote per precinct")
 
-plans.smc = redist_smc(map, nsims=5000, verbose=TRUE)
-plans.mergesplit = redist_mergesplit(map, nsims=5000, verbose=TRUE)
-plans.flip = redist_flip(map, nsims=5000, verbose=TRUE)
-plans.enum <- fl25_enum$plans[, fl25_enum$pop_dev < 0.1]
+plans.smc = redist_smc(map, nsims=10000, verbose=TRUE)
+plans.mergesplit = redist_mergesplit(map, nsims=10000, verbose=TRUE)
+plans.flip = redist_flip(map, nsims=10000, verbose=TRUE)
+fl25_enum <- redist.enumpart(map$adj,
+                              "data/unordered",
+                              "data/ordered",
+                              "data/fl25_enum",
+                              weight_path = "data/iowa_enum_pop",
+                              ndists = n,
+                              all = TRUE,
+                              lower = (1 - pop_tol) * sum(map$pop) / n,
+                              upper = (1 + pop_tol) * sum(map$pop) / n,
+                              total_pop = map$pop)
+plans.enum = fl25_enum$plans[, fl25_enum$parity < pop_tol]
 
+plans.clustering <- as.matrix(read.table("data/FLORIDA_25_FIXED")) + 1
+deviation <- function(col) {
+  max(
+    abs(sum(map$pop[col == 1]) * n / sum(map$pop) - 1),
+    abs(sum(map$pop[col == 2]) * n / sum(map$pop) - 1),
+    abs(sum(map$pop[col == 3]) * n / sum(map$pop) - 1),
+    abs(sum(map$pop[col == 4]) * n / sum(map$pop) - 1)
+  )
+}
+plans.clustering <- plans.clustering[, apply(plans.clustering, 2, deviation) < pop_tol]
+metrics.clustering <- redist.metrics(plans.clustering,
+                               c("EffGap", "DSeats", "Bias", "Responsiveness", "DVS", "Declination"),
+                               map$mccain,
+                               map$obama)
+metrics.clustering$method <- "Ours"
+metrics.clustering$draw <- unlist(lapply(metrics.clustering$draw, factor))
 metrics.enum <- redist.metrics(plans.enum,
                               c("EffGap", "DSeats", "Bias", "Responsiveness", "DVS", "Declination"),
                               map$mccain,
@@ -40,31 +68,30 @@ metrics.flip <- redist.metrics(plans.flip,
                                map$obama)
 metrics.flip$method <- "Flip"
 
-metrics <- bind_rows(metrics.flip, metrics.mergesplit, metrics.smc, metrics.enum)
+metrics <- bind_rows(metrics.clustering, metrics.flip, metrics.mergesplit, metrics.smc, metrics.enum)
 
 ggplot(metrics, aes(x = EffGap, linetype=factor(method), alpha=factor(method), fill=factor(method))) + 
   geom_density() +
-  scale_alpha_manual(values = c("Enum"=0.8, "Flip"=0.1, "MergeSplit"=0.1, "SMC"=0.1) ) +
+  scale_alpha_manual(values = c("Enum"=0.8, "Flip"=0.1, "MergeSplit"=0.1,"Ours"=0.1, "SMC"=0.1) ) +
   labs(alpha="Algorithm", linetype="Algorithm", fill="Algorithm",
-       title="Efficiency Gap distribution on the sampling space",
-       subtitle = "Big anomaly in MergeSplit out of the plotting range") +
-  xlab("Efficiency Gap") + xlim(-0.1, 0.2) +
+       title="Efficiency Gap distribution on the sampling space") +
+  xlab("Efficiency Gap") + xlim(-0.1, 0.45) +
   ylab("Density") 
 
 ggplot(metrics, aes(x = DVS, linetype=factor(method), alpha=factor(method), fill=factor(method))) + 
   geom_density() +
-  scale_alpha_manual(values = c("Enum"=0.8, "Flip"=0.1, "MergeSplit"=0.1, "SMC"=0.1) ) +
+  scale_alpha_manual(values = c("Enum"=0.8, "Flip"=0.1, "MergeSplit"=0.1,"Ours"=0.1, "SMC"=0.1) ) +
   labs(alpha="Algorithm", linetype="Algorithm", fill="Algorithm",
        title="Democratic Vote Share distribution on the sampling space") +
-  xlab("DVS") + xlim(0.3, 0.6)
+  xlab("DVS") + xlim(0.3, 0.65) +
   ylab("Density")
 
 ggplot(metrics, aes(x = Declination, linetype=factor(method), alpha=factor(method), fill=factor(method))) + 
   geom_density() +
-  scale_alpha_manual(values = c("Enum"=0.8, "Flip"=0.1, "MergeSplit"=0.1, "SMC"=0.1) ) +
+  scale_alpha_manual(values = c("Enum"=0.8, "Flip"=0.1, "MergeSplit"=0.1,"Ours"=0.1, "SMC"=0.1) ) +
   labs(alpha="Algorithm", linetype="Algorithm", fill="Algorithm",
        title="Declination distribution on the sampling space") +
-  xlab("Declination") + xlim(-0.2, 0.07) +
+  xlab("Declination") + xlim(-0.2, 0.45)
   ylab("Density")
 
 
--- plots_iowa.r
+++ plots_iowa.r
@@ -7,9 +7,11 @@ library(scales)
 
 data(iowa)
 dataset <- iowa
-map = redist_map(dataset, existing_plan = "cd_2010", pop_tol=0.01, total_pop = pop)
 
-mid=0.5
+pop_tol <- 0.2
+n <- 4
+mid <- 0.5
+map = redist_map(dataset, existing_plan = "cd_2010", pop_tol=pop_tol, total_pop = pop)
 redist.plot.map(map, fill = dem_08 / (dem_08 + rep_08)) + 
   scale_fill_gradient2(midpoint=mid, mid="white" , low="red", high="blue", space ="Lab") +
   labs(color="Democract vote share", title = "Democrat vote per precinct")
@@ -22,19 +24,30 @@ iowa_enum = redist.enumpart(map$adj,
                             "data/ordered",
                             "data/iowa_enum",
                             weight_path = "data/iowa_enum_pop",
-                            ndists = 4,
+                            ndists = n,
                             all = FALSE,
-                            n = 1000000,
+                            n = 1000,
+                            lower = (1 - pop_tol) * sum(map$pop) / n,
+                            upper = (1 + pop_tol) * sum(map$pop) / n,
                             total_pop = map$pop)
-plans.enum = iowa_enum$plans[, abs(iowa_enum$parity - 1) < 0.01]
+plans.enum <- iowa_enum$plans[, iowa_enum$parity < pop_tol]
 
-redist.plot.plans(plans.smc, draws=c("1", "2"),
-                  geom=map)
-redist.plot.plans(plans.mergesplit, draws=c("1", "2"),
-                  geom=map)
-redist.plot.plans(plans.flip, draws=c("1", "2"),
-                  geom=map)
-
+plans.clustering <- as.matrix(read.table("data/IOWA_FIXED")) + 1
+deviation <- function(col) {
+  max(
+    abs(sum(map$pop[col == 1]) * n / sum(map$pop) - 1),
+    abs(sum(map$pop[col == 2]) * n / sum(map$pop) - 1),
+    abs(sum(map$pop[col == 3]) * n / sum(map$pop) - 1),
+    abs(sum(map$pop[col == 4]) * n / sum(map$pop) - 1)
+  )
+}
+plans.clustering <- plans.clustering[, apply(plans.clustering, 2, deviation) < pop_tol]
+metrics.clustering <- redist.metrics(plans.clustering,
+                                     c("EffGap", "DSeats", "Bias", "Responsiveness", "DVS", "Declination"),
+                                     map$rep_08,
+                                     map$dem_08)
+metrics.clustering$method <- "Ours"
+metrics.clustering$draw <- unlist(lapply(metrics.clustering$draw, factor))
 metrics.enum <- redist.metrics(plans.enum,
                                c("EffGap", "DSeats", "Bias", "Responsiveness", "DVS", "Declination"),
                                map$rep_08,
@@ -57,38 +70,38 @@ metrics.flip <- redist.metrics(plans.flip,
                               map$dem_08)
 metrics.flip$method = "Flip"
 
-metrics <- bind_rows(metrics.flip, metrics.mergesplit, metrics.smc, metrics.enum)
+metrics <- bind_rows(metrics.clustering, metrics.flip, metrics.mergesplit, metrics.smc, metrics.enum)
 
 
 ggplot(metrics, aes(x = EffGap, linetype=factor(method), alpha=factor(method), fill=factor(method))) + 
   geom_density() +
-  scale_alpha_manual(values = c("Enum"=0.8, "Flip"=0.1, "MergeSplit"=0.1, "SMC"=0.1) ) +
+  scale_alpha_manual(values = c("Enum"=0.8, "Flip"=0.1, "MergeSplit"=0.1,"Ours"=0.1, "SMC"=0.1) ) +
   labs(alpha="Algorithm", linetype="Algorithm", fill="Algorithm",
        title="Efficiency Gap distribution on the sampling space") +
-  geom_vline(aes(xintercept=EffGap[Reduce("&", list(draw == "cd_2010", district==1, method=="Flip"))], color="cd_2010"), linetype="solid") +
-  scale_color_manual(name = "Plans", values=c(cd_2010="red")) +
-  xlab("Efficiency Gap") +
-  ylab("Density") + ylim(0,100)
+  #geom_vline(aes(xintercept=EffGap[Reduce("&", list(draw == "cd_2010", district==1, method=="Flip"))], color="cd_2010"), linetype="solid") +
+  #scale_color_manual(name = "Plans", values=c(cd_2010="red")) +
+  xlab("Efficiency Gap") + xlim(-0.5, -0.05) +
+  ylab("Density")
 
 ggplot(metrics, aes(x = DVS, linetype=factor(method), alpha=factor(method), fill=factor(method))) + 
   geom_density() +
-  scale_alpha_manual(values = c("Enum"=0.8, "Flip"=0.1, "MergeSplit"=0.1, "SMC"=0.1) ) +
+  scale_alpha_manual(values = c("Enum"=0.8, "Flip"=0.1, "MergeSplit"=0.1,"Ours"=0.1, "SMC"=0.1) ) +
   labs(alpha="Algorithm", linetype="Algorithm", fill="Algorithm",
        title="Democratic Vote Share distribution on the sampling space") +
-  geom_vline(aes(xintercept=DVS[Reduce("&", list(draw == "cd_2010", district==1, method=="Flip"))], color="cd_2010"), linetype="solid") +
-  scale_color_manual(name = "Plans", values=c(cd_2010="red")) +
-  xlab("DVS") +
+  #geom_vline(aes(xintercept=DVS[Reduce("&", list(draw == "cd_2010", district==1, method=="Flip"))], color="cd_2010"), linetype="solid") +
+  #scale_color_manual(name = "Plans", values=c(cd_2010="red")) +
+  xlab("DVS") + xlim(0.42, 0.65) +
   ylab("Density")
 
 ggplot(metrics, aes(x = Declination, linetype=factor(method), alpha=factor(method), fill=factor(method))) + 
   geom_density() +
-  scale_alpha_manual(values = c("Enum"=0.8, "Flip"=0.1, "MergeSplit"=0.1, "SMC"=0.1) ) +
+  scale_alpha_manual(values = c("Enum"=0.8, "Flip"=0.1, "MergeSplit"=0.1,"Ours"=0.1, "SMC"=0.1) ) +
   labs(alpha="Algorithm", linetype="Algorithm", fill="Algorithm",
        title="Declination distribution on the sampling space") +
-  geom_vline(aes(xintercept=Declination[Reduce("&", list(draw == "cd_2010", district==1, method=="Flip"))], color="cd_2010"), linetype="solid") +
-  scale_color_manual(name = "Plans", values=c(cd_2010="red")) +
-  xlab("Declination") + 
-  ylab("Density") + ylim(0, 30)
+  #geom_vline(aes(xintercept=Declination[Reduce("&", list(draw == "cd_2010", district==1, method=="Flip"))], color="cd_2010"), linetype="solid") +
+  #scale_color_manual(name = "Plans", values=c(cd_2010="red")) +
+  xlab("Declination") + xlim(0.13, -0.15)
+  ylab("Density") 
 
 ggplot(metrics, aes(x = Bias, fill=method)) + 
   geom_histogram() +
